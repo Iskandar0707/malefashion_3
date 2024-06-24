318f05a947db8940f32ec17da1974bfeeae37977
__N.2007.I_ <negmatoviskandar4@gmail.com>
2024-06-24 12:25:05 +0500

    finished orders -> admin,forms,models,urls,views.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from orders.models import OrderHistory
 
-# Register your models here.
+@admin.register(OrderHistory)
+class OrderHistoryAdmin(admin.ModelAdmin):
+    list_display = ("id", 'first_name', 'last_name', 'created_at')
+    list_display_links = ("id", 'first_name', 'last_name', 'created_at')
+    readonly_fields = ("first_name", 'last_name')
